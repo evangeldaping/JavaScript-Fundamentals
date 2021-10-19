0952676709b4756e20cb44337bf1f544ae1f3124
evangel <evangeldaping@gmail.com>
2021-10-19 14:23:14 +0800

    Async and Await

---
+++ Asynchronous JavaScript/Async and Await/sandbox.js
@@ -0,0 +1,16 @@
+// async and await
+
+const getTodos = async () => {
+    const response = await fetch('todos/luigi.json');
+    const data = await response.json();
+    return data;
+}
+
+console.log(1);
+console.log(2);
+
+getTodos()
+    .then(data =>    console.log('resolved:', data));
+
+console.log(3);
+console.log(4);
