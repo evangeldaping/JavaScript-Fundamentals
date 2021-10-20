5d5374454a178b1395083817567b107d58c0537d
evangel <evangeldaping@gmail.com>
2021-10-20 21:13:23 +0800

    Travresing the DOM (part 1)

---
+++ JavaScript DOM/Lesson 7/app.js
@@ -0,0 +1,6 @@
+const bookList = document.querySelector('#book-list');
+
+console.log('the parent node is:', bookList.parentNode);
+console.log('the parent element is:', bookList.parentElement.parentElement);
+
+console.log(bookList.children);
