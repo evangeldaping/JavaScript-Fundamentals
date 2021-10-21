e1d663d52b1ffa4c61c5ac3f790a3b5889a87605
evangel <evangeldaping@gmail.com>
2021-10-21 20:03:26 +0800

    Sorting and Ordering

--- JSON Server/js/index.js
+++ JSON Server/js/index.js
@@ -3,7 +3,7 @@
 const container = document.querySelector('.blogs');
 
 const renderPosts = async () => {
-    let url = 'http://localhost:3000/posts';
+    let url = 'http://localhost:3000/posts?_sort=likes&&_order=desc';
 
     const response = await fetch(url);
     const posts = await response.json();
