6fc8470a9d3e678a0540d57a914220836623b6aa
evangel <evangeldaping@gmail.com>
2021-10-22 17:17:06 +0800

    Create the components

---
+++ Simple Plain JavaScript Router/script.js
@@ -0,0 +1,44 @@
+// Components
+const HomeComponent = {
+  render: () => {
+    return `
+      <section>
+        <h1>Home</h1>
+        <p>This is just a test</p>
+      </section>
+    `;
+  }
+} 
+
+const Page1Component = {
+  render: () => {
+    return `
+      <section>
+        <h1>Page 1</h1>
+        <p>This is just a test</p>
+      </section>
+    `;
+  }
+} 
+
+const Page2Component = {
+  render: () => {
+    return `
+      <section>
+        <h1>Page 2</h1>
+        <p>This is just a test</p>
+      </section>
+    `;
+  }
+} 
+
+const ErrorComponent = {
+  render: () => {
+    return `
+      <section>
+        <h1>Error</h1>
+        <p>This is just a test</p>
+      </section>
+    `;
+  }
+}
